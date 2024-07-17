83331fe5ff034e0938002af82ee59bc061d18bdf
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-07-17 17:09:48 +0200

    feat(label): add Label.svelte component

---
+++ src/lib/components/atoms/Label/Label.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Typography } from '$lib/components';
+	import { type ILabelProps } from '$lib/types';
+
+	export let value: ILabelProps['value'] = '';
+</script>
+
+<div class="label">
+	<Typography variant="overline">{value}</Typography>
+</div>
+
+<style lang="less">
+	.label {
+		margin-bottom: 0.4rem;
+		width: fit-content;
+		opacity: 0.8;
+	}
+</style>
---
+++ src/lib/components/atoms/Label/Label.svelte.d.ts
@@ -0,0 +1,17 @@
+import { SvelteComponent } from 'svelte';
+
+declare const __propDef: {
+	props: {
+		value: string;
+	};
+	events: {
+		[evt: string]: CustomEvent;
+	};
+	slots: object;
+};
+type ILabelProps_ = typeof __propDef.props;
+export type { ILabelProps_ as ILabelProps };
+export type ILabelEvents = typeof __propDef.events;
+export type ILabelSlots = typeof __propDef.slots;
+
+export default class Label extends SvelteComponent<ILabelProps, ILabelEvents, ILabelSlots> {}
