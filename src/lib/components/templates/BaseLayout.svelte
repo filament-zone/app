d5aa5f239374d2be3998c4f77143fdd4e8c00e73
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-07-21 12:34:12 +0200

    fix(BaseLayout): fix base layout

--- src/lib/components/templates/BaseLayout.svelte
+++ src/lib/components/templates/BaseLayout.svelte
@@ -5,8 +5,10 @@
 
 <div class="flex flex-col h-svh">
 	<Header />
-	<div class="h-full p-4">
-		<slot />
+	<div class="flex w-full justify-center h-full">
+		<div class="h-full p-4 overflow-scroll w-full max-w-[1440px]">
+			<slot />
+		</div>
 	</div>
 	<Footer />
 </div>
