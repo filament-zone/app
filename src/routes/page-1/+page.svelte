f9cb4f41729c124d4550c6ea43798a36013d9761
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-07-26 13:15:39 +0200

    feat(page-1): add components to page-1

--- src/routes/page-1/+page.svelte
+++ src/routes/page-1/+page.svelte
@@ -1,5 +1,21 @@
 <script>
-	import { Typography } from '$lib/components';
+	import { Card, Table, TrendDisplay } from '$lib/components';
+
+	export let data;
 </script>
 
-<Typography>PAGE 1</Typography>
+<div class="flex flex-col w-full gap-4">
+	<div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+		{#each data.cards as card}
+			<Card label={card.label} class="w-full">
+				<TrendDisplay
+					value={card.data.value}
+					change={card.data.change}
+					period={card.data.period}
+					direction={card.data.direction}
+				/>
+			</Card>
+		{/each}
+	</div>
+	<Table {...data.tableData} />
+</div>
---
+++ src/routes/page-1/+page.ts
@@ -0,0 +1,213 @@
+import { flexRender } from '@tanstack/svelte-table';
+import { PlainLineChart, TableNetworkComponent, TrendDisplay } from '$lib/components';
+import FilamentLogo from '$lib/assets/logos/logo-filament.svg?component';
+import {
+	type IPlainLineChartProps,
+	type ITableNetworkComponentProps,
+	type ITableProps,
+	type ITrendDisplayProps
+} from '$lib/types';
+import { createSmoothRandomData, generateDateLabels } from '$lib/helpers';
+
+type TableData = {
+	id: string;
+	name: ITableNetworkComponentProps;
+	marketCap: ITrendDisplayProps;
+	tokenLiquidity: ITrendDisplayProps;
+	ibcTransfers: ITrendDisplayProps;
+	tvl: ITrendDisplayProps;
+	tvlChange: IPlainLineChartProps;
+};
+
+const labels = generateDateLabels('2022-12-01', '2022-12-31');
+const chartStyles = 'height: 60px; width: 160px';
+
+export async function load() {
+	const cards = [
+		{
+			label: 'Native Token Price',
+			data: {
+				value: '1 FILA ≈ $770.99',
+				change: '-3.32%',
+				period: '(24h)',
+				direction: 'down'
+			}
+		},
+		{
+			label: 'Market Cap',
+			data: {
+				value: '$380,654,380',
+				change: '-3.26%',
+				period: '(24h)',
+				direction: 'down'
+			}
+		},
+		{
+			label: 'Total Trading Volume (7d)',
+			data: {
+				value: '$5,090,487',
+				change: '26.36%',
+				period: '(7d)',
+				direction: 'up'
+			}
+		},
+		{
+			label: 'Staking APR',
+			data: {
+				value: '7.01%',
+				change: '-5.73%',
+				period: '(7d)',
+				direction: 'down'
+			}
+		}
+	];
+
+	const defaultData: TableData[] = [
+		{
+			id: '1',
+			name: { Icon: FilamentLogo, label: 'Filament Network', ticker: 'FILA' },
+			marketCap: {
+				value: '$11.12M',
+				change: '-3.32%',
+				period: '(24h)',
+				direction: 'down'
+			},
+			tokenLiquidity: {
+				value: '$11.12M',
+				change: '-3.32%',
+				period: '(7d)',
+				direction: 'down'
+			},
+			ibcTransfers: {
+				value: '$11.12M',
+				change: '-3.32%',
+				period: '(7d)',
+				direction: 'down'
+			},
+			tvl: {
+				value: '$11.12M',
+				change: '-3.32%',
+				period: '(7d)',
+				direction: 'down'
+			},
+			tvlChange: {
+				data: {
+					labels,
+					datasets: [
+						{
+							data: createSmoothRandomData(labels.length, 100, 120)
+						}
+					]
+				},
+				styles: chartStyles
+			}
+		},
+		{
+			id: '2',
+			name: { Icon: FilamentLogo, label: 'Filament Network', ticker: 'FILA' },
+			marketCap: {
+				value: '$300.12K',
+				change: '+4.22%',
+				period: '(24h)',
+				direction: 'up'
+			},
+			tokenLiquidity: {
+				value: '$300.12K',
+				change: '+4.22%',
+				period: '(7d)',
+				direction: 'up'
+			},
+			ibcTransfers: {
+				value: '$11.12M',
+				change: '-3.32%',
+				period: '(7d)',
+				direction: 'down'
+			},
+			tvl: {
+				value: '$11.12M',
+				change: '-3.32%',
+				period: '(7d)',
+				direction: 'down'
+			},
+			tvlChange: {
+				data: {
+					labels,
+					datasets: [
+						{
+							data: createSmoothRandomData(labels.length, 100, 120)
+						}
+					]
+				},
+				styles: chartStyles
+			}
+		}
+	];
+
+	const tableData: ITableProps = {
+		tableLabel: 'Table Label',
+		columnDef: [
+			{
+				accessorKey: 'id',
+				header: '#'
+			},
+			{
+				accessorKey: 'name',
+				header: 'Name',
+				cell: (info) => {
+					const value = info.getValue() as unknown as TableData['name'];
+					return flexRender(TableNetworkComponent, { ...value });
+				},
+				meta: {
+					cellStyle: {
+						['min-width']: '200px'
+					}
+				}
+			},
+			{
+				accessorKey: 'marketCap',
+				header: 'Market Cap',
+				cell: (info) => {
+					const value = info.getValue() as unknown as TableData['name'];
+					return flexRender(TrendDisplay, { ...value });
+				}
+			},
+			{
+				accessorKey: 'tokenLiquidity',
+				header: 'Token Liquidity',
+				cell: (info) => {
+					const value = info.getValue() as unknown as TableData['name'];
+					return flexRender(TrendDisplay, { ...value });
+				}
+			},
+			{
+				accessorKey: 'ibcTransfers',
+				header: 'IBC transfers(7d)',
+				cell: (info) => {
+					const value = info.getValue() as unknown as TableData['name'];
+					return flexRender(TrendDisplay, { ...value });
+				}
+			},
+			{
+				accessorKey: 'tvl',
+				header: 'TVL',
+				cell: (info) => {
+					const value = info.getValue() as unknown as TableData['name'];
+					return flexRender(TrendDisplay, { ...value });
+				}
+			},
+			{
+				accessorKey: 'tvlChange',
+				header: 'TVL Change(7d)',
+				cell: (info) => {
+					const value = info.getValue() as unknown as TableData['name'];
+					return flexRender(PlainLineChart, { ...value });
+				}
+			}
+		],
+		data: defaultData
+	};
+	return {
+		cards,
+		tableData
+	};
+}
