9a2aa26704dc754f4ed597b2737ed6a46eeff07d
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-08-08 10:43:18 +0200

    style(Card): minor style changes

--- src/lib/components/atoms/Card/Card.svelte
+++ src/lib/components/atoms/Card/Card.svelte
@@ -5,16 +5,17 @@
 	export let label: ICardProps['label'] = '';
 </script>
 
-<div class={`card ${$$props.class} `}>
+<div class={`card ${$$props.class} p-2`}>
 	<Typography variant="caption">{label}</Typography>
 	<slot />
 </div>
 
 <style lang="less">
 	.card {
+		display: flex;
+		flex-direction: column;
 		box-shadow: 0 0 0 0.1px var(--primary-white);
 		background-color: var(--content1);
-		padding: 10px;
 		border-radius: 4px;
 	}
 </style>
