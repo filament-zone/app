33c0ce9d89d032b0b6672f5fc2970e66fa89e72b
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-08-08 13:47:58 +0200

    fix(ValidatorsTable): update TableValidatorOptionsComponent.svelte component

--- src/lib/components/molecules/Table/components/TableValidatorOptionsComponent/TableValidatorOptionsComponent.svelte
+++ src/lib/components/molecules/Table/components/TableValidatorOptionsComponent/TableValidatorOptionsComponent.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
-	import { Button } from '$lib/components';
-	import { EModalVariant, type ITableNetworkComponentProps } from '$lib/types';
 	import { modalStore } from '$lib/features';
+	import { Button, List, MenuPopover } from '$lib/components';
+	import MoreHorizontalIcon from '$lib/assets/icons/more-horizontal.svg?component';
+	import {
+		EModalVariant,
+		type IListProps,
+		type ITableValidatorOptionsComponentProps
+	} from '$lib/types';
 
-	export let validatorId: ITableNetworkComponentProps['Icon'];
+	export let validatorId: ITableValidatorOptionsComponentProps['validatorId'];
 
 	const { openModal } = modalStore;
 
 	const handleStake = () => {
 		openModal({ variant: EModalVariant.VALIDATOR_STAKE, state: { validatorId } });
 	};
+
+	const options: IListProps['options'] = [
+		{ value: 'stake', label: 'Stake', onClick: handleStake },
+		{ value: 'redelegate', label: 'Redelegate', disabled: true },
+		{ value: 'unstake', label: 'Unstake', disabled: true },
+		{ value: 'claimRewards', label: 'Claim Rewards', disabled: true }
+	];
 </script>
 
 <div class="flex flex-row items-center">
-	<Button on:click={handleStake}>Stake</Button>
+	<Button on:click={handleStake} class="mr-4">Stake</Button>
+	<MenuPopover>
+		<div slot="trigger" class="cursor-pointer">
+			<MoreHorizontalIcon />
+		</div>
+		<List slot="content" {options} />
+	</MenuPopover>
 </div>
