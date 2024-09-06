3bfc045f6721e0cd977795152c80953f45f5c9cd
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-09-06 18:12:13 +0200

    feat(campaign-creation): add base structure for air-drop second step

--- src/lib/features/campaign-creation/campaigns-steps/air-drop/campaign-air-drop-step-2.svelte
+++ src/lib/features/campaign-creation/campaigns-steps/air-drop/campaign-air-drop-step-2.svelte
@@ -1,5 +1,40 @@
 <script lang="ts">
-	import { Typography } from '$lib/components';
+	import { Container, Dropdown, Input, Table, Typography } from '$lib/components';
+	import { EDropdownSizeVariant, EInputSizeVariant } from '$lib/types';
 </script>
 
-<Typography>AirDrop Step-2</Typography>
+<div class="flex flex-col gap-5">
+	<Container label="General">
+		<div class="flex flex-col gap-5">
+			<Typography variant="caption"
+				>On this page you can design the airdrop criteria by allocating weights to specific user
+				actions. Please start by selecting a start date and the number of snapshots to create.
+			</Typography>
+			<div class="flex flex-row justify-between">
+				<Input
+					label="Snapshot Start Date"
+					placeholder="04  /  01  /  2024"
+					sizeVariant={EInputSizeVariant.MEDIUM}
+				/>
+				<Dropdown label="Shapshot Interval" sizeVariant={EDropdownSizeVariant.MEDIUM} />
+				<Input
+					label="Total Snapshots"
+					placeholder="10"
+					type="number"
+					sizeVariant={EInputSizeVariant.MEDIUM}
+				/>
+			</div>
+		</div>
+	</Container>
+	<Container label="Eligibility Criteria">
+		<div class="flex flex-col gap-5">
+			<Typography variant="caption">
+				Via criterions you can define specific actions of a user that you want to reward with your
+				airdrop. By allocating a weight to the actions, you can adjust the impact of each action on
+				the users Airdrop allocation. Add criterions and click on the edit button of each item to
+				configure the weights as preferred.
+			</Typography>
+			<Table />
+		</div>
+	</Container>
+</div>
