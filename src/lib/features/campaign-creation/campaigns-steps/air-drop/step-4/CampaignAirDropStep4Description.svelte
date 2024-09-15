0f613eea0c812ff787f6cc6b73fb5541dca401f8
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-09-15 15:31:57 +0200

    feat(campaign-creation): update Summary step

--- src/lib/features/campaign-creation/campaigns-steps/air-drop/step-4/CampaignAirDropStep4Description.svelte
+++ src/lib/features/campaign-creation/campaigns-steps/air-drop/step-4/CampaignAirDropStep4Description.svelte
@@ -1,97 +1,11 @@
 <script lang="ts">
+	import { campaignStore } from '$lib/features';
 	import { Typography } from '$lib/components';
+
+	const { campaignDetails } = campaignStore;
 </script>
 
 <div>
-	<Typography variant="h5">Project Overview</Typography>
-	<Typography variant="h6">
-		EtherLend is a cutting-edge decentralized lending protocol designed to bring efficient and
-		transparent lending to the Ethereum ecosystem. With EtherLend, users can lend and borrow assets
-		in a trustless manner, securing their funds via smart contracts while enjoying the benefits of
-		decentralized finance (DeFi). Our mission is to simplify lending processes, reduce gas fees, and
-		provide innovative features that empower both lenders and borrowers in the DeFi space.
-	</Typography>
-	<br />
-	<Typography variant="h6">
-		Built on Ethereum, EtherLend leverages robust security protocols and offers an intuitive
-		interface that ensures a seamless user experience. Our protocol aims to drive mass adoption of
-		decentralized lending by providing a reliable, flexible, and secure environment for users of all
-		levels of experience.
-	</Typography>
-	<br />
-	<Typography variant="h5">Airdrop Campaign Overview</Typography>
-	<Typography variant="h6">
-		To celebrate our upcoming launch, EtherLend is excited to announce our EtherLend Genesis
-		Airdrop, designed to reward our early adopters and supporters. As we gear up for our full launch
-		on Ethereum, this airdrop is an opportunity to distribute tokens to those who believe in our
-		vision and actively participate in the DeFi ecosystem.
-	</Typography>
-	<br />
-	<Typography variant="h6">
-		The EtherLend Genesis Airdrop will distribute a total of 10 million ELND tokens (10% of the
-		total supply) to eligible users. By participating in our ecosystem, users can earn a portion of
-		this allocation based on their engagement with lending, borrowing, and liquidity provision
-		activities.
-	</Typography>
-	<br />
-	<Typography variant="h5">Eligibility Criteria</Typography>
-	<Typography variant="h6">
-		Eligibility for the EtherLend Genesis Airdrop is based on several DeFi-related activities across
-		the Ethereum network. These criteria include:
-		<br />
-		1. Lending ETH or other assets: Users who have lent assets to various Ethereum-based DeFi protocols
-		over the past 6 months.
-		<br />
-		2. Borrowing Assets: Users who have borrowed assets through Ethereum-based protocols.
-		<br />
-		3. Providing Liquidity: Participants in Ethereum liquidity pools, particularly in EtherLend or partner
-		pools.
-		<br />
-		4. Holding ELND: Early backers and token holders of our native ELND token.
-	</Typography>
-	<br />
-	<Typography variant="h6">
-		Each criterion will be weighted to reflect the user’s contribution to the DeFi ecosystem, and
-		token rewards will be allocated based on activity level and engagement.
-	</Typography>
-	<br />
-	<Typography variant="h5">Vision & Methodology</Typography>
-	<Typography variant="h6">
-		At EtherLend, we believe that decentralization and financial autonomy are the future. The
-		EtherLend Genesis Airdrop is not just a distribution of tokens—it’s an invitation to help shape
-		the future of decentralized lending. By rewarding early contributors, we aim to foster a
-		thriving community that will actively participate in the governance and growth of the EtherLend
-		protocol.
-	</Typography>
-	<br />
-	<Typography variant="h6">
-		Through this airdrop, we are building a foundation of decentralized ownership. ELND holders will
-		have governance rights within the protocol, allowing them to propose and vote on key decisions,
-		from protocol upgrades to new asset listings.
-	</Typography>
-	<br />
-	<Typography variant="h6">
-		This airdrop campaign is just the first step in our broader strategy to reward active
-		participants in the DeFi space and foster a user-centric, decentralized future. We are committed
-		to building a protocol that serves the community’s needs, and we invite you to join us in this
-		journey as we redefine what decentralized lending can achieve.
-	</Typography>
-	<br />
-	<Typography variant="h5">How to participate</Typography>
-	<Typography variant="h6">
-		Eligibility for the EtherLend Genesis Airdrop is based on several DeFi-related activities across
-		the Ethereum network. These criteria include:
-		<br />
-		1. Snapshot: We will take a series of snapshots between January 1st, 2024 and March 31st, 2024 to
-		assess user eligibility.
-		<br />
-		2. Claim Process: Eligible users will be able to claim their airdrop directly from the FIlament Hub
-		upon our full launch. Full instructions will be provided via our official channels.
-	</Typography>
-	<br />
-	<Typography variant="h6">
-		For more detailed information about our airdrop and protocol, visit etherlend.io.
-		<br />
-		Join the EtherLend revolution and help shape the future of decentralized finance on Ethereum!
-	</Typography>
+	<Typography variant="h4">Description:</Typography>
+	<Typography variant="h5">{$campaignDetails.description ?? ''}</Typography>
 </div>
