89b69b45af102fdfa401876729270a6488290af8
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-09-26 12:40:19 +0200

    feat(helper): add capitalizeFirstLetter helper

--- src/lib/helpers/string.helpers.ts
+++ src/lib/helpers/string.helpers.ts
@@ -4,3 +4,8 @@ export const shortCutTransactionHash = (tx: string): string => {
 export const bigIntToHexId = (value: bigint): string => {
 	return `0x${value.toString(16)}`;
 };
+
+export const capitalizeFirstLetter = (str: string): string => {
+	if (!str) return str;
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
