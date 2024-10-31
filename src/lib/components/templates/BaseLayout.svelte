180db28895e27f3eba7622dd320c30fde39cee31
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-10-31 11:56:36 +0100

    feat(layout): fix base layout

--- src/lib/components/templates/BaseLayout.svelte
+++ src/lib/components/templates/BaseLayout.svelte
@@ -3,13 +3,11 @@
 	import { Header, Footer } from '$lib/components';
 </script>
 
-<div class="base-layout flex flex-row h-svh">
-	<div class="flex flex-col w-full h-fit items-center">
+<div class="flex flex-row h-svh">
+	<div class="flex flex-col w-full">
 		<Header />
-		<div
-			class="base-layout-body flex flex-col w-full max-w-[1440px] lg:max-w-[1280px] justify-center overflow-auto overflow-x-hidden min-h-svh h-fit"
-		>
-			<div class="px-4 py-4 md:px-10 flex flex-col w-full min-h-svh h-fit pt-[12]">
+		<div class="flex w-full justify-center overflow-auto overflow-x-hidden h-full">
+			<div class="p-4 w-full max-w-[1440px]">
 				<slot />
 			</div>
 		</div>
