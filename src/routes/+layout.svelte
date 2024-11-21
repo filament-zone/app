006b49907d2790e48137c79e64a897861a6dc305
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-11-21 11:38:30 +0100

    feat(wallet): revert placeholder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
 	import { onMount } from 'svelte';
 	import { Chart, registerables } from 'chart.js';
 	import { ModalController, navigationStore, walletStore } from '$lib/features';
-	import { BaseLayout, Typography } from '$lib/components';
+	import { BaseLayout } from '$lib/components';
 	import { readFromLocalStorage } from '$lib/utils';
 	import { EWalletProvider } from '$lib/services';
 	import '$lib/styles/app.less';
 
 	const { navigating, previousPage } = navigationStore;
-	const { initializeWallet, wallet } = walletStore;
+	const { initializeWallet } = walletStore;
 
 	navigating.subscribe(() => {
 		if ($navigating?.from?.url.pathname) {
@@ -27,12 +27,6 @@
 </script>
 
 <BaseLayout>
-	{#if !$wallet.connected}
-		<div class="flex justify-center items-center h-full">
-			<Typography>Please connect your wallet to continue.</Typography>
-		</div>
-	{:else}
-		<slot />
-	{/if}
+	<slot />
 </BaseLayout>
 <ModalController />
