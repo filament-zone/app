3a09d548ae8da7b4a6ffd3bc0e9be4a98381f327
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2024-12-16 10:19:29 +0100

    feat(storybook): update storybook deps

--- src/lib/features/campaign/components/ModalCampaignVote/ModalCampaignVote.stories.svelte
+++ src/lib/features/campaign/components/ModalCampaignVote/ModalCampaignVote.stories.svelte
@@ -1,9 +1,9 @@
 <script context="module">
 	import { Story, Template } from '@storybook/addon-svelte-csf';
-	import { ModalCampaignDeposit } from '$lib/features';
+	import { ModalCampaignVote } from '$lib/features';
 
 	export const meta = {
-		title: 'Features/Modal/Campaign Deposit'
+		title: 'Features/Modal/Campaign Vote'
 	};
 </script>
 
@@ -11,4 +11,4 @@
 	<svelte:component this={args.modal} {...args} />
 </Template>
 
-<Story name="Primary" args={{ modal: ModalCampaignDeposit }} />
+<Story name="Primary" args={{ modal: ModalCampaignVote }} />
