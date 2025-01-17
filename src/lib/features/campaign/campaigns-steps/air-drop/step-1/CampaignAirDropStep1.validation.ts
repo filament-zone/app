063bacf7bced9b1607c0cc97bf017b9d3154d10b
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2025-01-17 14:39:15 +0100

    fix(validation): improve error messages for CampaignAirDropStep1 validation

--- src/lib/features/campaign/campaigns-steps/air-drop/step-1/CampaignAirDropStep1.validation.ts
+++ src/lib/features/campaign/campaigns-steps/air-drop/step-1/CampaignAirDropStep1.validation.ts
@@ -1,12 +1,14 @@
 import * as yup from 'yup';
 
 export const campaignAirDropStep1ValidationSchema = yup.object().shape({
-	title: yup.string().max(20, 'Should be less than 20 characters').required('Title is required'),
+	title: yup
+		.string()
+		.max(20, 'Title should be less than 20 characters')
+		.required('Title is required'),
 	description: yup
 		.string()
-		.max(50, 'Should be less than 50 characters')
+		.max(50, 'Description should be less than 50 characters')
 		.required('Description is required'),
-
 	evictions: yup
 		.array()
 		.min(1, 'You must evict at least 1 delegate')
